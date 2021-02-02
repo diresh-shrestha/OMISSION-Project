d7a9ebd9000cf256d77236e3d191e939e9839f35
diresh-shrestha <direshshrestha95@gmail.com>
2021-02-02 14:04:52 -0600

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container } from '../utils/styles'
+
+const About = () => {
+  return (
+    <Container>
+      <h1>Mission</h1>
+      <p>
+        Our mission is to organize a community to engage with journalism and
+        celebrate the work of individuals in the field. Through the medium of
+        clothing, we aim to put garments in conversation with media ecology;
+        bringing awareness to media literacy and the contemporary conditions of
+        the digital landscape.
+      </p>
+    </Container>
+  )
+}
+
+export default About
