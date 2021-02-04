f9dba374444c02e108cff58f6647c8c9ab95ff19
diresh-shrestha <direshshrestha95@gmail.com>
2021-02-04 11:42:13 -0600

    updated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/OMISSION_logo.png`, // This path is relative to the root of the site.
+        icon: `src/images/ommission.svg`, // This path is relative to the root of the site.
       },
     },
     {
