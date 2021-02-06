791366c4b41528625c892e63b565a8a36dbbf0ea
diresh-shrestha <direshshrestha95@gmail.com>
2021-02-06 16:03:23 -0600

    added social links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { StyledFooter, FooterImg } from './styles'
+import { StyledFooter, FooterImg, SocialContianer, SocialLinks } from './styles'
 import FooterLogo from './Footer_logo.png'
 import { Link } from 'gatsby'
+import Facebook from '../Icons/Facebook'
+import Instragram from '../Icons/Instagram'
 
 const Footer = () => {
   return (
@@ -9,6 +11,14 @@ const Footer = () => {
       <Link to="/">
         <FooterImg src={FooterLogo} alt="OMISSION Project Alternate Logo" />
       </Link>
+      <SocialContianer>
+        <SocialLinks href="">
+          <Facebook />
+        </SocialLinks>
+        <SocialLinks href="">
+          <Instragram />
+        </SocialLinks>
+      </SocialContianer>
       <p> © {new Date().getFullYear()} - OMISSION Project</p>
     </StyledFooter>
   )
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -6,8 +6,25 @@ export const StyledFooter = styled.footer`
   justify-content: space-between;
   margin-top: 5rem;
   height: 50px;
+  margin-bottom: 2rem;
+  padding: 1rem;
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+    justify-content: center;
+  }
 `
 export const FooterImg = styled.img`
   height: 50px;
   width: 80px;
+`
+
+export const SocialContianer = styled.div`
+  display: flex;
+`
+
+export const SocialLinks = styled.a`
+  margin: auto 0.5rem;
+  text-decoration: none;
+  color: black;
 `
