c88b90f9e84d9185399e4d0c64226f81b1cfb18f
diresh-shrestha <direshshrestha95@gmail.com>
2021-02-12 12:22:42 -0600

    updated seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,33 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import 'normalize.css'
 
 import SEO from '~/components/seo'
 import ProductGrid from '~/components/ProductGrid'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <>
-    <SEO keywords={[`clothing`, `ecommerce`, `shopify`, `journalism`]} />
+    <SEO
+      title="Home"
+      image={data.seoImg.childImageSharp.fluid}
+      keywords={[`clothing`, `ecommerce`, `shopify`, `journalism`]}
+    />
     <ProductGrid />
   </>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  {
+    seoImg: file(relativePath: { eq: "Images/omission.png" }) {
+      id
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
