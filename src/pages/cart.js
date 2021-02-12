3f27a16cbdac279f40868396343c4b2f201a43fd
diresh-shrestha <direshshrestha95@gmail.com>
2021-02-12 12:35:26 -0600

    added seo

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -3,12 +3,16 @@ import React from 'react'
 import Cart from '~/components/Cart'
 import { Container } from '~/utils/styles'
 import 'normalize.css'
+import SEO from '../components/seo'
 
 const CartPage = () => (
-  <Container style={{ maxWidth: `1500px` }}>
-    <h1>Cart</h1>
-    <Cart />
-  </Container>
+  <>
+    <SEO title="Cart" />
+    <Container style={{ maxWidth: `1500px` }}>
+      <h1>Cart</h1>
+      <Cart />
+    </Container>
+  </>
 )
 
 export default CartPage
